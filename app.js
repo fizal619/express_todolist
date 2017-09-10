729fdefffa1396f46cb154ad10bb59bad31343f5
Fizal Sarif <fizal.sarif@gmail.com>
2017-09-10 13:37:18 -0400

    add icon and public folder

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ let todos = [
   }
 ]
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
