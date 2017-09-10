d280b7e0ad39f202f0c02c9f09760600a1f26ee7
Fizal Sarif <fizal.sarif@gmail.com>
2017-09-10 12:28:52 -0400

    I can add todos yay

--- app.js
+++ app.js
@@ -3,18 +3,44 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-
+let todos = [
+  {
+    item: "do laundry",
+    done: true
+  },
+  {
+    item: "do more laundry",
+    done: false
+  }
+]
 
 app.set('views', './views')
-app.set('view engine', 'ejss')
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
+app.get('/todo/new', (req, res)=>{
+  res.render('new_item')
+})
+
+app.post('/todo/new', (req, res)=>{
+  console.log(req.body.item)
+  let newTodo = {
+    item: req.body.item,
+    done: false
+  }
+  todos.push(newTodo)
+  console.log(todos)
+  res.redirect('/')
+})
+
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    things: todos
+  })
 })
 
 
-app.listen(process.env.PORT, () => {
+app.listen(3000, () => {
   console.log('All systems go on 3000')
 })
